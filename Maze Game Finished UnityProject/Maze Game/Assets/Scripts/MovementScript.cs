46f066c0cdf1d6b39085c0ddd85564befdbf1b6a
scott.mather <scott.mather@mindcandy.com>
2014-02-19 11:16:34 +0000

    Initial commit of Maze Game tutorial

---
+++ Maze Game Finished UnityProject/Maze Game/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	
+	public float speed = 4;
+
+	private Vector2 movement;
+	private Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update()
+	{
+		float inputX = Mathf.Round(Input.GetAxis("Horizontal"));
+		float inputY = Mathf.Round(Input.GetAxis("Vertical"));
+
+		if(inputY == 0 && inputX == 0)
+		{
+			animator.SetBool("Idle",true);
+		}
+		else
+		{
+			animator.SetBool("Idle",false);
+		}
+
+		movement = new Vector2(
+			speed * inputX,
+			speed * inputY);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+		animator.SetFloat("Horizontal",movement.x);
+		animator.SetFloat("Vertical",movement.y);
+	}
+}
---
+++ Maze Game UnityProject/Maze Game/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	
+	public float speed = 4;
+	
+	private Vector2 movement;
+	//private Animator animator;
+	
+	void Start()
+	{
+		//animator = GetComponent<Animator>();
+	}
+	
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		movement = new Vector2(
+			speed * inputX,
+			speed * inputY);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+		//animator.SetFloat("Horizontal",movement.x);
+		//animator.SetFloat("Vertical",movement.y);
+	}
+}
